3e51e8da390482a842f342896aea5daf024d7343
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-07 15:05:53 +0200

    test: add test for /tea endpoint with param and test

--- src/controllers/tea_request.test.ts
+++ src/controllers/tea_request.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "../app";
 
 describe("Test tea API endpoint request", () => {
-  test("GET /tea without param should return correct default object", async () => {
+  test("GET /tea without param should return correct default object with default tea name", async () => {
     const res = await request(app).get("/tea");
 
     expect(res.statusCode).toEqual(200);
@@ -10,5 +10,11 @@ describe("Test tea API endpoint request", () => {
       drinkType: "Tea",
       name: "Green",
     });
+  });
+
+  test("GET /tea with param should return correct object", async () => {
+    const res = await request(app).get("/tea").query({ teaName: "Herbal" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ drinkType: "Tea", name: "Herbal" });
   });
 });
